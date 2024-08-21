bc2aea45f196fcea405ba6bcef1628a6f729f549
vaibhavvaja <vajaakapitbull@gmail.com>
2024-08-21 09:25:57 +0100

    dummy user and user component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {}
