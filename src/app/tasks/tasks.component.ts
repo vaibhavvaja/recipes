0b295dd1574a719954ed172f059218f29e3f4022
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-08-21 15:18:02 +0100

    new task component added

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,17 +1,18 @@
-import { Component, Input } from "@angular/core";
-import { TaskComponent } from "../task/task.component";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: "app-tasks",
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: "./tasks.component.html",
   styleUrl: "./tasks.component.css",
 })
 export class TasksComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) name!: string;
-
+  addNewTask = false;
   tasks = [
     {
       id: "t1",
@@ -44,5 +45,9 @@ export class TasksComponent {
 
   onCompleteTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onAddNewTask() {
+    this.addNewTask = true;
   }
 }
