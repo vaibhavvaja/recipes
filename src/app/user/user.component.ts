7ce616a4ba7cbd182340b90be1bb7a082f36ec8d
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-08-21 14:39:33 +0100

    task component created , dummy tasks added

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,6 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
+import { User } from "./user.model";
 
 @Component({
   selector: "app-user",
