b2ef9e3b3a08fd7ef1bd72d1196df609a5a17365
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-08-21 10:18:13 +0100

    users list displayed

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { DUMMY_USERS } from "../dummy-users";
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  imports: [CommonModule],
+  templateUrl: "./user.component.html",
+  styleUrl: "./user.component.css",
 })
 export class UserComponent {
+  @Input({ required: true }) name!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) imagePath!: string;
 
+  users = DUMMY_USERS;
+
+  onSelectUser() {}
 }
