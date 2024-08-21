1dbb876c37723906e9d1147c2264a1aaee92e8df
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-08-21 12:49:34 +0100

    use of @For and @If else instead of *ngFor and *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Output } from "@angular/core";
-import { CommonModule } from "@angular/common";
 
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
@@ -9,13 +8,13 @@ import { TasksComponent } from "./tasks/tasks.component";
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, UserComponent, CommonModule, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId: string = "";
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,15 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
-import { CommonModule } from "@angular/common";
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: "app-user",
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: "./user.component.html",
   styleUrl: "./user.component.css",
 })
@@ -14,15 +20,13 @@ export class UserComponent {
   // imagePath = computed(() => "assets/users/" + this.avatar());
   // select = output<string>();
 
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) name!: string;
-  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return "assets/users/" + this.avatar;
+    return "assets/users/" + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
