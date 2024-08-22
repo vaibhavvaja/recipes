b007ac50c74d137ecc769bf46d49a2e3a9c509b0
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-08-22 14:43:58 +0100

    tasks saved and updating in local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -29,6 +29,11 @@ export class TasksService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem("tasks");
+    if (tasks) this.tasks = JSON.parse(tasks);
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => userId === task.userId);
   }
@@ -41,9 +46,15 @@ export class TasksService {
       summary: task.summary,
       dueDate: task.dueDate,
     });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(this.tasks));
   }
 }
