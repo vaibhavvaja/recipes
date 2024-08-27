271198d514f7b527bfbddcb1036315f937e15975
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-08-27 09:54:58 +0100

    migrated all components to module from standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { UserComponent } from "./user/user.component";
+import { CardComponent } from "./shared/card/card.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { TaskComponent } from "./tasks/task/task.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TaskComponent,
+    NewTaskComponent,
+    TasksComponent,
+    CardComponent,
+  ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+  imports: [BrowserModule, FormsModule],
 })
 export class AppModule {}
--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,12 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrl: "./card.component.css",
 })
-export class CardComponent {
-
-}
+export class CardComponent {}
