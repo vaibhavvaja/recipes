26e1e92b9e98c464ae22dc54b4c7d20b56c452c8
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-08 11:18:11 +0100

    components seperated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+import { HeaderComponent } from "./header/header.component";
+import { ServerStatusComponent } from "./server-status/server-status.component";
+import { TrafficComponent } from "./traffic/traffic.component";
+import { SupportTicketsComponent } from "./support-tickets/support-tickets.component";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  templateUrl: './app.component.html',
+  imports: [
+    HeaderComponent,
+    ServerStatusComponent,
+    TrafficComponent,
+    SupportTicketsComponent,
+  ],
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  dummyTrafficData = [
-    {
-      id: 'd1',
-      value: 433,
-    },
-    {
-      id: 'd2',
-      value: 260,
-    },
-    {
-      id: 'd3',
-      value: 290,
-    },
-    {
-      id: 'd4',
-      value: 410,
-    },
-    {
-      id: 'd5',
-      value: 397,
-    },
-    {
-      id: 'd6',
-      value: 488,
-    },
-    {
-      id: 'd47',
-      value: 589,
-    },
-  ];
-  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
-  currentStatus = 'online';
-}
+export class AppComponent {}
---
+++ src/app/support-tickets/support-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-support-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './support-tickets.component.html',
+  styleUrl: './support-tickets.component.css'
+})
+export class SupportTicketsComponent {
+
+}
