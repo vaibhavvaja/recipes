d1e6040097fb018212ac3bb8b2888abf5baf6971
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-08 21:33:14 +0100

    dashboard item reusable component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 
 import { HeaderComponent } from "./header/header.component";
-import { ServerStatusComponent } from "./server-status/server-status.component";
-import { TrafficComponent } from "./traffic/traffic.component";
-import { SupportTicketsComponent } from "./support-tickets/support-tickets.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { SupportTicketsComponent } from "./dashboard/support-tickets/support-tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: "app-root",
@@ -13,6 +14,7 @@ import { SupportTicketsComponent } from "./support-tickets/support-tickets.compo
     ServerStatusComponent,
     TrafficComponent,
     SupportTicketsComponent,
+    DashboardItemComponent,
   ],
   templateUrl: "./app.component.html",
 })
