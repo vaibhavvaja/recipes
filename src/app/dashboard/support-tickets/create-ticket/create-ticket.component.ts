22ab11ae83c910241624d1836d52d3bd80220e81
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-09-18 16:26:56 +0100

    button component reused from shared folder using ng-content and class selector for ng-content

--- src/app/dashboard/support-tickets/create-ticket/create-ticket.component.ts
+++ src/app/dashboard/support-tickets/create-ticket/create-ticket.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { ButtonComponent } from "../../../shared/button/button.component";
 
 @Component({
   selector: "app-create-ticket",
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: "./create-ticket.component.html",
   styleUrl: "./create-ticket.component.css",
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { ButtonComponent } from "../shared/button/button.component";
 
 @Component({
   selector: "app-header",
   standalone: true,
+  imports: [ButtonComponent],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
 })
