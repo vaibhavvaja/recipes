2fb1bc4db77975e9776bab5c6458bfbc226c022d
vajav <vaibhav.vaja@admiralgroup.co.uk>
2024-09-19 10:19:34 +0100

    shared component control added for similar form control fields

--- src/app/dashboard/support-tickets/create-ticket/create-ticket.component.ts
+++ src/app/dashboard/support-tickets/create-ticket/create-ticket.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { ButtonComponent } from "../../../shared/button/button.component";
+import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: "app-create-ticket",
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ControlComponent],
   templateUrl: "./create-ticket.component.html",
   styleUrl: "./create-ticket.component.css",
 })
