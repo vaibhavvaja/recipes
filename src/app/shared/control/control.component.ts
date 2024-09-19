b577d7cb93e7f409218e2355f44825c6464f141b
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-19 16:40:59 +0100

    hostbinding and hostlistner added and commented

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  HostBinding,
+  HostListener,
+  input,
+  ViewEncapsulation,
+} from "@angular/core";
 
 @Component({
   selector: "app-control",
@@ -9,8 +15,14 @@ import { Component, input, ViewEncapsulation } from "@angular/core";
   encapsulation: ViewEncapsulation.None,
   host: {
     class: "control",
+    "(click)": "onClick()",
   },
 })
 export class ControlComponent {
+  // @HostBinding("class") className = "control";
+  // @HostListener("click")
+  onClick() {
+    console.log("hello");
+  }
   label = input.required<string>();
 }
