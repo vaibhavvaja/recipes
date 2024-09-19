5fc46821f385540763dbbad214d3992a65b371ac
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-19 23:23:48 +0100

    viewChild singal and decorator used for controlling form programatically

--- src/app/dashboard/support-tickets/create-ticket/create-ticket.component.ts
+++ src/app/dashboard/support-tickets/create-ticket/create-ticket.component.ts
@@ -1,12 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, viewChild, ViewChild } from "@angular/core";
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-create-ticket",
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: "./create-ticket.component.html",
   styleUrl: "./create-ticket.component.css",
 })
-export class CreateTicketComponent {}
+export class CreateTicketComponent {
+  // @ViewChild("form") form?: ElementRef<HTMLFormElement>;
+  form = viewChild.required<ElementRef<HTMLFormElement>>("form");
+
+  onSubmit(title: string, description: string) {
+    console.log(title);
+    console.log(description);
+    console.log(this.form);
+    this.form()?.nativeElement.reset();
+  }
+}
