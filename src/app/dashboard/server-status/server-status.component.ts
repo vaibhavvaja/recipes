4866c45ffafdcd96bb5bc42606c7faaf0860edd0
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-19 21:26:01 +0100

    server status randomise with ngOnInit

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-server-status",
@@ -7,6 +7,17 @@ import { Component } from "@angular/core";
   templateUrl: "./server-status.component.html",
   styleUrl: "./server-status.component.css",
 })
-export class ServerStatusComponent {
-  currentStatus = "online";
+export class ServerStatusComponent implements OnInit {
+  currentStatus: "online" | "offline" | "unknown" = "offline";
+
+  constructor() {}
+
+  ngOnInit() {
+    setInterval(() => {
+      const random = Math.random();
+
+      this.currentStatus =
+        random < 0.5 ? "online" : random < 0.9 ? "offline" : "unknown";
+    }, 5000);
+  }
 }
