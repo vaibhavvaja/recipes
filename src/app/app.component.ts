40e5435b4391ed6a7925c6c7f9aae093e9940656
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-21 11:42:19 +0100

    ngIf added - structural directive with help of computed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { AuthComponent } from './auth/auth.component';
-import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthComponent } from "./auth/auth.component";
+import { LearningResourcesComponent } from "./learning-resources/learning-resources.component";
+import { AuthService } from "./auth/auth.service";
+import { Permission } from "./auth/auth.model";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  imports: [AuthComponent, LearningResourcesComponent],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
+  imports: [AuthComponent, LearningResourcesComponent, CommonModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission() === "admin");
+}
