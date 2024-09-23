d2b364672fac6b10435e781b26800f198a1a5779
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-23 15:01:45 +0100

    initial code with few updates using template driven approach

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
+
+@Component({
+  selector: "app-login",
+  standalone: true,
+  templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css",
+  imports: [FormsModule],
+})
+export class LoginComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
