f1fc988105325f170df8b3cd44705b311b0382f6
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-25 16:46:30 +0100

    sorting added with queryParams and singal

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,22 +3,30 @@ import { Component, computed, inject, input } from "@angular/core";
 import { TaskComponent } from "./task/task.component";
 import { Task } from "./task/task.model";
 import { TasksService } from "./tasks.service";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-tasks",
   standalone: true,
   templateUrl: "./tasks.component.html",
   styleUrl: "./tasks.component.css",
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
-  taskService = inject(TasksService);
-
   userId = input.required<string>();
+  order = input<"asc" | "desc">();
+  taskService = inject(TasksService);
 
   userTasks = computed(() => {
     return this.taskService
       .allTasks()
-      .filter((task) => task.userId === this.userId());
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === "asc") {
+          return a.id > b.id ? -1 : 1;
+        } else {
+          return a.id > b.id ? 1 : -1;
+        }
+      });
   });
 }
