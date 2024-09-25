205cd8f6595d396831daea4a765fec3c7269b20d
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-25 15:43:23 +0100

    user routes file added for child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,7 @@ import { Routes } from "@angular/router";
 
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { TasksComponent } from "./tasks/tasks.component";
+import { routes as userRoutes } from "./users/users.routes";
 import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
@@ -11,11 +10,7 @@ export const routes: Routes = [
   {
     path: "users/:userId",
     component: UserTasksComponent,
-    children: [
-      { path: "", redirectTo: "tasks", pathMatch: "full" },
-      { path: "tasks", component: TasksComponent },
-      { path: "tasks/new", component: NewTaskComponent },
-    ],
+    children: userRoutes,
   },
   {
     path: "**",
