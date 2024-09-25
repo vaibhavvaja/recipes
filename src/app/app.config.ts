40c245207d208505a2d3384daa32ffbfff3b2d1d
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-25 11:09:55 +0100

    initial routes setup and router-oulet and routerlink used

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { provideRouter } from "@angular/router";
+import { ApplicationConfig } from "@angular/core";
+
+import { routes } from "./app.routes";
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes)],
+};
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+
+import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+
+export const routes: Routes = [
+  { path: "", component: NoTaskComponent },
+  { path: "tasks", component: TasksComponent },
+];
