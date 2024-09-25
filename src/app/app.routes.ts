77ed0acbfbbf9ad87a145b67900dcdb562cee027
vaibhavvaja <vajaakapitbull@gmail.com>
2024-09-25 11:36:18 +0100

    using activatedRoute and paramMap observable to get the url param and update the UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from "@angular/router";
 
-import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 
 export const routes: Routes = [
   { path: "", component: NoTaskComponent },
-  { path: "tasks", component: TasksComponent },
+  { path: "users/:userId", component: UserTasksComponent },
 ];
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+
+import { UsersService } from "../users.service";
+import { HttpParams } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-user-tasks',
+  selector: "app-user-tasks",
   standalone: true,
-  templateUrl: './user-tasks.component.html',
-  styleUrl: './user-tasks.component.css',
+  templateUrl: "./user-tasks.component.html",
+  styleUrl: "./user-tasks.component.css",
 })
-export class UserTasksComponent {}
+export class UserTasksComponent implements OnInit {
+  user = "";
+
+  constructor(
+    private usersService: UsersService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe({
+      next: (val) => {
+        this.user =
+          this.usersService.users.find((u) => u.id === val.get("userId"))
+            ?.name ?? "";
+      },
+    });
+  }
+}
